91ff6bf1b10ec02341e780fcd7c6d0bd5d97d20c
Yug Marwaha <yugmarwaha987@gmail.com>
2026-03-27 00:00:09 -0500

    Initial setup

---
+++ src/main/java/com/docrouter/doc_router/DocRouterApplication.java
@@ -0,0 +1,13 @@
+package com.docrouter.doc_router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocRouterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docrouter/doc_router/DocRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docrouter.doc_router;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocRouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
